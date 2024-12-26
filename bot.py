f123d0b09d2875798f0af8c9995ab05f7999c389
akshmangal005 <akashmangal79@gmail.com>
2024-12-26 22:18:23 +0530

    refactor

--- bot.py
+++ bot.py
@@ -4,34 +4,33 @@ import json
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 from instagrapi import Client
 
-
-
 instagram_username = os.getenv("INSTAGRAM_USERNAME")
 instagram_password = os.getenv("INSTAGRAM_PASSWORD")
 
-# Check if credentials are set
-if not instagram_username or not instagram_password:
-    raise ValueError("Username and password must be set in environment variables.")
+def check_messages():
 
-print("Login trying")
-cl = Client()
-cl.login(instagram_username, instagram_password)
-print("Login Successful")
+    if not instagram_username or not instagram_password:
+        raise ValueError("Username and password must be set in environment variables.")
 
-li = []
-try:
-    print("Checking new messages")
-    thread = cl.direct_threads(20, "unread")
-    if(len(thread) > 0):
-        my_chat = thread[0]
-        thread_id = my_chat.id
-        if hasattr(my_chat, 'messages'):
-            for item in my_chat.messages:
-                li.append(item.clip.video_url)
+    print("Login trying")
+    cl = Client()
+    cl.login(instagram_username, instagram_password)
+    print("Login Successful")
+
+    li = []
+    try:
+        print("Checking new messages")
+        thread = cl.direct_threads(20, "unread")
+        if(len(thread) > 0):
+            my_chat = thread[0]
+            thread_id = my_chat.id
+            if hasattr(my_chat, 'messages'):
+                for item in my_chat.messages:
+                    li.append(item.clip.video_url)
+            else:
+                print("Message field not found")
+            cl.direct_thread_hide(thread_id) #delete the entire chat
         else:
-            print("Message field not found")
-        cl.direct_thread_hide(thread_id) #delete the entire chat
-    else:
-        print("No new messages")
-except:
-    print("Error occured while checking new messages")
+            print("No new messages")
+    except:
+        print("Error occured while checking new messages")
