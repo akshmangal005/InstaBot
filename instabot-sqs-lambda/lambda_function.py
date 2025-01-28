e0a4dd94f7449ea2b31831086cdb701c6e50df13
akshmangal005 <akashmangal79@gmail.com>
2025-01-28 22:45:10 +0530

    restructure

--- instabot-sqs-lambda/lambda_function.py
+++ instabot-sqs-lambda/lambda_function.py
@@ -5,6 +5,7 @@ from instagrapi import Client
 instagram_username = os.getenv("INSTAGRAM_USERNAME")
 instagram_password = os.getenv("INSTAGRAM_PASSWORD")
 
+url_list = []
 def check_messages():
     if not instagram_username or not instagram_password:
         raise ValueError("Username and password must be set in environment variables.")
@@ -22,7 +23,7 @@ def check_messages():
             thread_id = my_chat.id
             if hasattr(my_chat, 'messages'):
                 for item in my_chat.messages:
-                    li.append(item.clip.video_url)
+                    url_list.append(item.clip.video_url)
                 cl.direct_thread_hide(thread_id)      #delete the entire chat
             else:
                 print("Message field not found")
@@ -31,7 +32,7 @@ def check_messages():
     except:
         print("Error occured while checking new messages")
 
-li = []
-check_messages()
-# for item in li:
-    # prepare a queue and send the item to the second lambda
+
+def lambda_handler(event, context):
+    check_messages()
+    
