8f28a28c2366d71d2c95e586f6c1cad6048ef726
akshmangal005 <akashmangal79@gmail.com>
2025-02-26 23:28:48 +0530

    added-retry-mechanism-for-searching-song

--- instabot_search_engine/lambda_function.py
+++ instabot_search_engine/lambda_function.py
@@ -30,14 +30,14 @@ def download_audio(url, output_file):
     except requests.exceptions.RequestException as e:
         print(f"Failed to download the audio: {e}")
 
-def trim_audio(input_file, output_file, duration=5):
+def trim_audio(input_file, output_file, start = 0, duration=5):
     try:
         output_dir = os.path.dirname(output_file)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
         audio = AudioSegment.from_file(input_file)
-        trimmed_audio = audio[:duration * 1000] 
+        trimmed_audio = audio[start * 1000: (start + duration) * 1000] 
         trimmed_audio.export(output_file, format="mp3")
 
         # print(f"Audio trimmed to {duration} seconds and saved as {output_file}")
@@ -90,16 +90,24 @@ def fetch_song(base64_string):
     return parsed_data.get('track', {}).get('title', 'Title not found')
 
 def search_song(id, audio_url):
-    original_file = f"../../testing/tmp/{id}_original.mp3"
-    trimmed_file = f"../../testing/tmp/{id}_trimmed.mp3"
-    mono_file = f"../../testing/tmp/{id}_mono.raw"
-    download_audio(audio_url, original_file)
-    trim_audio(original_file, trimmed_file)
 
-    convert_to_mono(trimmed_file, mono_file)
+    for retry in range(0, 15, 5):
+        print(f'''Attempting {retry// 5 + 1} try for Searching Music.''')
+        original_file = f"../../testing/tmp/{id}_original.mp3"
+        trimmed_file = f"../../testing/tmp/{id}_trimmed.mp3"
+        mono_file = f"../../testing/tmp/{id}_mono.raw"
+        if(retry == 0):
+            download_audio(audio_url, original_file)
+        trim_audio(original_file, trimmed_file, retry)
 
-    base64_string = convert_mp3_to_base64(mono_file)
+        convert_to_mono(trimmed_file, mono_file)
 
-    song_name = fetch_song(base64_string)
+        base64_string = convert_mp3_to_base64(mono_file)
+
+        song_name = fetch_song(base64_string)
+        if song_name == 'Title not found':
+            continue
+        else:
+            break
     return song_name
-
+    
